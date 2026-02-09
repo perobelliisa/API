d54c83ec02c9aeee46c9d33ee50186688991d18c
Isadora Perobelli <isagperobelli@gmail.com>
2026-02-09 17:09:57 -0300

    Add files via upload

---
+++ perobelli/Api_vaida_certo/config.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'nossa_humilde_chave_secreta'
+DEBUG = True
+
+DB_HOST = 'localhost'
+DB_NAME = r'C:\Users\Aluno\Desktop\perobelli\BANCO.FDB'
+
+DB_USER = 'sysdba'
+DB_PASSWORD = 'sysdba'
+
